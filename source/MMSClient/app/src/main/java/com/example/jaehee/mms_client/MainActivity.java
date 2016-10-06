2d2865adfad29278d092efd5698a904166604967
HaJaehee <lovesm135@naver.com>
2016-10-06 17:31:39 +0900

    android client project upload
    android client project upload

---
+++ source/MMSClient/app/src/main/java/com/example/jaehee/mms_client/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.jaehee.mms_client;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView m_TxtvReceivedMsg;
+    public static String m_StrReceivedMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        m_TxtvReceivedMsg = (TextView) findViewById(R.id.TxtvReceivedMsg);
+
+        new AsyncTask<Void,Void,Void>(){
+
+            private String strUrl;
+            private String result;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                strUrl = "http://143.248.57.72:8080/HelloWorldServer/HelloWorld"; //탐색하고 싶은 URL이다.
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try{
+                    URL Url = new URL(strUrl);  // URL화 한다.
+                    HttpURLConnection conn = (HttpURLConnection) Url.openConnection(); // URL을 연결한 객체 생성.
+                    conn.setRequestMethod("GET"); // get방식 통신
+                    conn.setDoOutput(true);       // 쓰기모드 지정
+                    conn.setDoInput(true);        // 읽기모드 지정
+                    conn.setUseCaches(false);     // 캐싱데이터를 받을지 안받을지
+                    conn.setDefaultUseCaches(false); // 캐싱데이터 디폴트 값 설정
+
+                    String strCookie = conn.getHeaderField("Set-Cookie"); //쿠키데이터 보관
+
+                    InputStream is = conn.getInputStream();        //input스트림 개방
+
+                    StringBuilder builder = new StringBuilder();   //문자열을 담기 위한 객체
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));  //문자열 셋 세팅
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line+ "\n");
+                    }
+
+                    result = builder.toString();
+
+
+
+                }catch(MalformedURLException | ProtocolException exception) {
+                    exception.printStackTrace();
+                }catch(IOException io){
+                    io.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                m_StrReceivedMsg = result;
+            }
+        }.execute();
+
+        m_TxtvReceivedMsg.setText(m_StrReceivedMsg);
+    }
+}
+
